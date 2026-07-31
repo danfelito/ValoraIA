a6ca11218f9041331ece37673abd6c9fb55e4265
danfelito <danfelavicas@gmail.com>
2026-07-31 12:05:21 -0600

    Conectar frontend con Supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+window.VALORAIA_CONFIG = {
+  supabaseUrl: 'https://wctwkhixmmluckingjhf.supabase.co',
+  supabasePublishableKey: 'sb_publishable_M2oDnNBIQiQeu2w5sANx1g_Iw1dS0ju',
+  storageBucket: 'valuation-documents'
+};
